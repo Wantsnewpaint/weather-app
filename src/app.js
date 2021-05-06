9ab6de0a232f36f4fdd32d5d3a861263fa71aa05
Wantsnewpaint <80902812+Wantsnewpaint@users.noreply.github.com>
2021-05-06 10:42:39 -0500

    added Fahrenheit conversion

--- src/app.js
+++ src/app.js
@@ -40,7 +40,8 @@ function displayTemperature(response) {
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = response.data.name;
   let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  temperature = response.data.main.temp;
+  temperatureElement.innerHTML = Math.round(temperature);
   let descriptionElement = document.querySelector("#description");
   descriptionElement.innerHTML = response.data.weather[0].description;
   let humidity = document.querySelector("#humidity");
@@ -64,7 +65,28 @@ function handleSubmit(event) {
   search(searchCityElement.value);
 }
 
-search("Montevideo");
-
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
+
+function displayFahrenheit(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round((temperature * 9) / 5 + 32);
+}
+
+function displayCelsius(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+let temperature = null;
+
+let fahrenheitLink = document.querySelector("#fahrenheit");
+fahrenheitLink.addEventListener("click", displayFahrenheit);
+
+let celsiusLink = document.querySelector("#celsius");
+celsiusLink.addEventListener("click", displayCelsius);
+
+
+search("Montevideo");
